1675981d8361935af00a2ead3db6299365f30184
Ed Bradbury <ebrad005@gold.ac.uk>
2021-07-05 13:38:51 +0100

    Can't serve Vue app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Import the Express library
 const express = require('express');
 
-const serveStatic = require('serve-static');
+// const serveStatic = require('serve-static');
 
 // Create new Express app instance
 const app = express();
@@ -16,7 +16,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // Specify the directory holding the page templates which will be returned to clients
-app.use(serveStatic(`${__dirname}/client/dist`));
+// app.use(serveStatic(`${__dirname}/client/dist`));
+app.use(express.static(`${__dirname}/client/dist`));
 
 // // Configure rendering engine
 // app.set('view engine', 'ejs');
